17fbe936a2c95666005d8651b1735ee978a0fa0a
erikhoy <erikshoy@gmail.com>
2020-01-18 18:44:09 -0700

    cleaned up format; added validation;

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -1,19 +1,20 @@
-<?php include("includes/init.php"); ?>
-<?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
-<?php
+<?php 
+  include("includes/init.php");
 
-if(empty($_GET['id'])) {
+  if (!$session->is_signed_in()) {
+    redirect("login.php");
+  }
+
+  if (empty($_GET['id'])) {
     redirect("photos.php");
-}
+  }
 
-$photo = Photo::find_by_id($_GET['id']);
+  $photo = Photo::find_by_id(filter_var($_GET['id'], FILTER_SANITIZE_INT));
 
-if($photo) {
+  if($photo) {
     $photo->delete_photo();
     $session->message("The photo {$photo->filename} has been deleted");
     redirect("photos.php");
-} else {
-    redirect("photos.php");
-}
-
-?>
+  }
+  
+  redirect("photos.php");
