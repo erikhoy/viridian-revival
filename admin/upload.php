cfc23b17bda4d2feed73a6697053eb3c539942b2
erikhoy <erikshoy@gmail.com>
2020-01-19 14:44:49 -0700

    cleaned up format; added validation;

--- admin/upload.php
+++ admin/upload.php
@@ -1,68 +1,64 @@
-<?php include("includes/header.php"); ?>
-<?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
 <?php
-    $message = "";
-    if(isset($_FILES['file'])) {
-        $photo = new Photo();
-        $photo->title = $_POST['title'];
-        $photo->set_file($_FILES['file']);
+  include("includes/header.php");
 
-        if($photo->save()) {
-            $message = "Photo uploaded successfully";
-        } else {
-            $message = join("<br>", $photo->errors);
-        }
+  if (!$session->is_signed_in()) {
+    redirect("login.php");
+  }
+
+  $message = "";
+  
+  if (isset($_FILES['file'])) {
+    $photo = new Photo();
+    $photo->title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+    $photo->set_file($_FILES['file']);
+    if ($photo->save()) {
+      $message = "Photo uploaded successfully";
+    } else {
+      $message = join("<br>", $photo->errors);
     }
+  }
 ?>
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            
-            <?php include("includes/top_nav.php"); ?>
 
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <?php include("includes/side_nav.php"); ?>
-            <!-- /.navbar-collapse -->
-        </nav>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <?php include("includes/top_nav.php"); ?>
+  <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+  <?php include("includes/side_nav.php"); ?>
+<!-- /.navbar-collapse -->
+</nav>
 
-
-
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Upload</h1>
-                        <div class="row">
-                            <div class="col col-md-6">
-                                <?php echo $message; ?>
-                                <form action="" method="post" enctype="multipart/form-data">
-                                    <div class="form-group">
-                                        <input type="text" name="title" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="file" name="file">
-                                    </div>
-                                    <input type="submit" name="submit">
-                                </form>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col col-lg-12">
-                                <form action="upload.php" class="dropzone"></form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
+<div id="page-wrapper">
+  <div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">Upload</h1>
+        <div class="row">
+          <div class="col col-md-6">
+            <?=$message; ?>
+            <form action="" method="post" enctype="multipart/form-data">
+              <div class="form-group">
+                <input type="text" name="title" class="form-control">
+              </div>
+              <div class="form-group">
+                <input type="file" name="file">
+              </div>
+              <input type="submit" name="submit">
+            </form>
+          </div>
         </div>
-        <!-- /#page-wrapper -->
+        <div class="row">
+          <div class="col col-lg-12">
+            <form action="upload.php" class="dropzone"></form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
 
-  <?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php");
