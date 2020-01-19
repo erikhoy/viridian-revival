461e16607450ac50503d4efaaf4eb74dca28bc5b
erikhoy <erikshoy@gmail.com>
2020-01-18 18:46:21 -0700

    cleaned up format; added validation;

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -1,18 +1,20 @@
-<?php include("includes/init.php"); ?>
-<?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
-<?php
-	if(empty($_GET['id'])) {
-	    redirect("users.php");
-	}
+<?php 
+  include("includes/init.php");
 
-	$user = User::find_by_id($_GET['id']);
+  if (!$session->is_signed_in()) {
+    redirect("login.php");
+  }
 
-	if($user) {
-	    $user->delete_photo();
-	    $session->message("The user {$user->username} has been deleted.");
-	    redirect("users.php");
+  if (empty($_GET['id'])) {
+    redirect("users.php");
+  }
 
-	} else {
-	    redirect("users.php");
-	}
-?>
+  $user = User::find_by_id(filter_var($_GET['id'], FILTER_SANITIZE_INT));
+
+  if ($user) {
+    $user->delete_photo();
+    $session->message("The user {$user->username} has been deleted.");
+    redirect("users.php");
+  }
+
+  redirect("users.php");
