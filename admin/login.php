fd31f321a17f3f33edd8e38a9bd5bb6e4946ec99
erikhoy <erikshoy@gmail.com>
2020-01-19 12:44:34 -0700

    cleaned up format;

--- admin/login.php
+++ admin/login.php
@@ -1,42 +1,43 @@
-<?php require_once("includes/header.php"); ?>
-<?php
-	if($session->is_signed_in()) {
-		redirect("index.php");
-	}
+<?php 
+  require_once("includes/header.php");
 
-	if(isset($_POST['submit'])) {
-		$username = trim($_POST['username']);
-		$password = trim($_POST['password']);
+  if ($session->is_signed_in()) {
+    redirect("index.php");
+  }
 
-		//method to check database
-		$user_found = User::verify_user($username, $password);
+  if (isset($_POST['submit'])) {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
 
-		if($user_found) {
-			$session->login($user_found);
-			redirect("index.php");
+    //method to check database
+    $user_found = User::verify_user($username, $password);
 
-		} else {
-			$message = "Your password or username are incorrect.";
-		}
-	} else {
-		$username = "";
-		$password = "";
-		$message  = "";
-	}
+    if ($user_found) {
+      $session->login($user_found);
+      redirect("index.php");
+    } 
+	  
+    $message = "Your password or username are incorrect.";
+  } else {
+    $username = "";
+    $password = "";
+    $message  = "";
+  }
 ?>
+
 <div class="col-md-4 col-md-offset-3">
-	<h4 class="bg-danger"><?php echo $message; ?></h4>
-	<form id="login-id" action="" method="post">
-		<div class="form-group">
-			<label for="username">Username</label>
-			<input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
-		</div>
-		<div class="form-group">
-			<label for="password">Password</label>
-			<input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
-		</div>
-		<div class="form-group">
-			<input type="submit" name="submit" value="Submit" class="btn btn-primary">
-		</div>
-	</form>
+  <h4 class="bg-danger"><?=$message; ?></h4>
+  <form id="login-id" action="" method="post">
+    <div class="form-group">
+      <label for="username">Username</label>
+      <input type="text" class="form-control" name="username" value="<?=htmlentities($username); ?>" >
+    </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" name="password" value="<?=htmlentities($password); ?>">
+    </div>
+    <div class="form-group">
+      <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+    </div>
+  </form>
 </div>
