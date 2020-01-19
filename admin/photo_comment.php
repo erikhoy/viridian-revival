43d4dd221475a675e45779bfa4c24f17ebb5aef6
erikhoy <erikshoy@gmail.com>
2020-01-19 12:54:01 -0700

    cleaned up format; added validation;

--- admin/photo_comment.php
+++ admin/photo_comment.php
@@ -1,70 +1,64 @@
-<?php include("includes/header.php"); ?>
-<?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
-<?php 
-if(empty($_GET['id'])) {
-    redirect("photos.php");
-}
+<?php
+  include("includes/header.php");
 
-$comments = Comment::find_the_comments($_GET['id']);
+  if (!$session->is_signed_in()) {
+    redirect("login.php");
+  }
+
+  if (empty($_GET['id'])) {
+    redirect("photos.php");
+  }
+
+  $comments = Comment::find_the_comments(filter_var($_GET['id'], FILTER_SANITIZE_INT));
 
 ?>            
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            
-            <?php include("includes/top_nav.php"); ?>
 
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <?php include("includes/side_nav.php"); ?>
-            <!-- /.navbar-collapse -->
-        </nav>
-
-
-
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Users</h1>
-                        <p class="bg-success"><?php echo $message; ?></p>
-                        <div class="col col-md-12">
-                            <table class="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Author</th>
-                                        <th>Body</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach($comments as $comment) : ?>
-                                    <?php //$filename = SITE_ROOT.DS.'admin'.DS.'images'.DS.$comment->filename; ?>
-                                    <tr>
-                                        <td><?php echo $comment->id; ?>
-                                            <div class="action_links">
-                                                <a href="delete_comment_photo.php?id=<?php echo $comment->id; ?>">Delete</a>
-                                            </div>
-                                        </td>
-                                        <td><?php echo $comment->author; ?></td>
-                                        <td><?php echo $comment->body; ?></td>
-                                    </tr>
-                                    <?php endforeach; ?>
-                                </tbody>
-                            </table>
-                            
-                        </div>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <?php include("includes/top_nav.php"); ?>
+  <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+  <?php include("includes/side_nav.php"); ?>
+<!-- /.navbar-collapse -->
+</nav>
+<div id="page-wrapper">
+  <div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">Users</h1>
+        <p class="bg-success"><?=$message; ?></p>
+        <div class="col col-md-12">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Author</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($comments as $comment) : ?>
+                <tr>
+                  <td>
+                    <?=$comment->id; ?>
+                    <div class="action_links">
+                      <a href="delete_comment_photo.php?id=<?=$comment->id; ?>">Delete</a>
                     </div>
-                </div>
-                <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
+                  </td>
+                  <td><?=$comment->author; ?></td>
+                  <td><?=$comment->body; ?></td>
+                </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
         </div>
-        <!-- /#page-wrapper -->
+      </div>
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
 
-  <?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php");
